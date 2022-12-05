ae7cbf5ad7962c87a8036ea37643f01bfc19565d
Jitsem <jitse.meulenijzer@gmail.com>
2022-12-05 19:40:51 +0100

    Day 3 - Part 2

--- day_three/src/main.rs
+++ day_three/src/main.rs
@@ -2,16 +2,14 @@ const RUGSACKS: &str = include_str!("../data/input.txt");
 
 fn main() {
     let mut total_prio = 0;
-    for (nr, line) in RUGSACKS.lines().enumerate() {
+    for line in RUGSACKS.lines() {
         if line.len() % 2 != 0 {
             panic!("Rugsack did not contain even nr of items")
         }
-        println!("Rugsack {}: {}", nr, line);
 
         let (first, second) = line.split_at(line.len() / 2);
         for char in first.chars() {
             if second.contains(char) {
-                println!("Duplicate: {}", char);
                 if !char.is_alphabetic() {
                     panic!("Unexpected char in rugsack")
                 }
@@ -20,12 +18,35 @@ fn main() {
                     false => u32::from(char) - 38,
                 };
 
-                println!("Prio: {}", prio);
                 total_prio += prio;
                 break;
             }
         }
     }
-    println!("Total Prio: {}", total_prio);
+    println!("Total Prio part 1: {}", total_prio);
 
+    let mut total_prio_2 = 0;
+
+    for ((one, two), three) in RUGSACKS
+        .lines()
+        .step_by(3)
+        .zip(RUGSACKS.lines().skip(1).step_by(3))
+        .zip(RUGSACKS.lines().skip(2).step_by(3))
+    {
+        for char in one.chars() {
+            if two.contains(char) && three.contains(char) {
+                if !char.is_alphabetic() {
+                    panic!("Unexpected char in rugsack")
+                }
+                let prio: u32 = match char.is_lowercase() {
+                    true => u32::from(char) - 96,
+                    false => u32::from(char) - 38,
+                };
+
+                total_prio_2 += prio;
+                break;
+            }
+        }
+    }
+    println!("Total Prio part 2: {}", total_prio_2);
 }
